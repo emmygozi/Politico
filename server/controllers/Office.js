b6d755947ff31ef7acd11cb8dab18149754b2bf5
emmygozi <mygitmail@yahoo.com>
2019-01-30 22:57:06 +0100

    [Feature #163539317] Admin can create an office

---
+++ server/controllers/Office.js
@@ -0,0 +1,30 @@
+import offices from '../models/office';
+
+class Office {
+  static createOffice(req, res) {
+    const { type, name } = req.body;
+    const id = offices[offices.length - 1].id + 1;
+
+    const aNewOffice = {
+      id, type, name
+    };
+
+    const alreadyCreatedOffice = offices.find(
+      searchValue => searchValue.type.toLowerCase() === type.toLowerCase()
+    );
+
+    if (alreadyCreatedOffice) {
+      return res.status(409).json({
+        status: 409,
+        error: `An office with '${type}' already exists`
+      });
+    }
+    offices.push(aNewOffice);
+    res.status(201).json({
+      status: 201,
+      data: [offices[offices.length - 1]]
+    });
+  }
+}
+
+export default Office;
