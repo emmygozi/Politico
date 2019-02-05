33b27ebb2e6648e76188267efc1db8893940d192
emmygozi <mygitmail@yahoo.com>
2019-02-05 12:11:15 +0100

    [Chore #163708000] Implement PR feedback to remove connection duplication

--- server/controllers/Party.js
+++ server/controllers/Party.js
@@ -2,9 +2,8 @@ import pool from '../db/config';
 
 class Party {
   static async getAll(req, res) {
-    const client = await pool.connect();
     const { rows } = await pool.query('SELECT * FROM parties ORDER BY id LIMIT 10');
-    client.release();
+
     res.status(200).json({ status: 200, data: rows });
   }
 
@@ -12,11 +11,9 @@ class Party {
     const { name } = req.body;
     const { id } = req.params;
 
-    const client = await pool.connect();
     const { rows } = await pool.query(`UPDATE parties SET name = '${name}'
     WHERE id = '${id}' RETURNING  id, name`);
 
-    client.release();
     if (rows.length === 0) {
       return res.status(404).json({
         status: 404,
@@ -37,11 +34,9 @@ class Party {
       rowMode: 'array',
     };
 
-    const client = await pool.connect();
     const alreadyCreatedParty = await pool.query(partyAlreadyRegistered);
 
     if (alreadyCreatedParty.rows[0] !== undefined) {
-      client.release();
       return res.status(409).json({
         status: 409,
         error: `A party with '${name}' already exists`
@@ -50,8 +45,6 @@ class Party {
 
     const { rows } = await pool.query(`INSERT INTO parties (name, hqAddress, logoUrl)
     VALUES ('${name}', '${hqAddress}', '${logoUrl}') RETURNING * `);
-
-    client.release();
 
     res.status(201).json({
       status: 201,
@@ -62,9 +55,7 @@ class Party {
   static async getOneParty(req, res) {
     const { id } = req.params;
 
-    const client = await pool.connect();
     const { rows } = await pool.query(`SELECT * FROM parties WHERE id = '${id}'`);
-    client.release();
     if (rows.length === 0) {
       return res.status(404)
         .json({ status: 404, error: 'The party requested does not exist' });
@@ -76,11 +67,9 @@ class Party {
   static async removeAPoliticalParty(req, res) {
     const { id } = req.params;
 
-    const client = await pool.connect();
     const { rows } = await pool.query(`DELETE FROM parties
      WHERE id = '${id}' RETURNING *`);
 
-    client.release();
     if (rows.length === 0) return res.status(404).json({ status: 404, error: 'The party requested does not exist' });
 
     res.status(200).json({ status: 200, data: [{ message: 'Requested party sucessfully deleted' }] });
