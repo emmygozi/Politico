54a817215c232c4fd19489b9bc680de2fdaa52f6
Emmanuel <mygitmail@yahoo.com>
2019-07-28 20:47:58 +0100

    [Feat #164535598] get all party

--- server/controllers/Party.js
+++ server/controllers/Party.js
@@ -2,10 +2,7 @@ import pool from '../db/config';
 
 class Party {
   static async getAll(req, res) {
-    const { rows } = await pool.query(`select parties.id as id, parties.name as name, 
-    parties.logoUrl, parties.hqAddress, parties.dateadded, 
-    candidates.party as candidate from parties 
-    inner join candidates on parties.id=candidates.party ORDER BY id LIMIT 10`);
+    const { rows } = await pool.query('select * from parties');
 
     res.status(200).json({ status: 200, data: rows });
   }
